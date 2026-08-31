bbcfebdb7534cc696f3fa0fa43ebda219c47af3f
Harsha ds <harsha072004@gmail.com>
2026-08-31 16:05:26 +0530

    first commit

---
+++ src/main/java/com/e/toHowManyIapplied/ToHowManyIappliedApplication.java
@@ -0,0 +1,13 @@
+package com.e.toHowManyIapplied;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToHowManyIappliedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToHowManyIappliedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e/toHowManyIapplied/ToHowManyIappliedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e.toHowManyIapplied;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToHowManyIappliedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
